d3288730f6e1e56186d7cbfec043fd9735191f5a
Xavier Watkins <xavier.watkins@gmail.com>
2018-05-18 13:28:29 +0100

    Adding persists so subsequent calls still work

--- mappingsService.js
+++ mappingsService.js
@@ -11,7 +11,7 @@ class MappingsService {
 
     initMock() {
         this.mock = nock(baseUrl);
-        this.mock.get('/gifts/mappings').reply(200, mappings);
+        this.mock.persist().get('/gifts/mappings').reply(200, mappings);
     }
 
 }
